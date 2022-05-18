51ba2b6f77a27f6ac75d404f8d0d458162003679
Yousuf Boutahar El Maachi <yusef_boutahar@hotmail.com>
2022-05-18 20:13:44 +0100

    adding middleware autentificacion firebase

--- api/publics/index.js
+++ api/publics/index.js
@@ -2,12 +2,13 @@ const router = require('express').Router();
 const controller = require('./publics.controller');
 const multer = require('multer');
 const upload = multer();
+const middleware = require('./../../midlewares/auth')
 
-router.get('/', controller.getAllPublics);
-router.get('/:id', controller.getPublicById);
-router.delete('/:id', controller.deletePublic);
-router.post('/', upload.single('image'), controller.addPublic);
-router.patch('/:id', upload.single('image'), controller.updatePublic);
+router.get('/', middleware.authFirebase ,controller.getAllPublics);
+router.get('/:id', middleware.authFirebase ,controller.getPublicById);
+router.delete('/:id', middleware.authFirebase ,controller.deletePublic);
+router.post('/', middleware.authFirebase ,upload.single('image'), controller.addPublic);
+router.patch('/:id', middleware.authFirebase ,upload.single('image'), controller.updatePublic);
 
 
 module.exports = router;
